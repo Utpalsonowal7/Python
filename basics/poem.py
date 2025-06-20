5f506381250ee28729549d538167f9a855c93332
Utpal Sonowal <132993823+Utpalsonowal7@users.noreply.github.com>
2025-06-20 01:37:22 +0000

    multi lines print

---
+++ basics/poem.py
@@ -0,0 +1,12 @@
+print(''' 
+Said my illness
+I’m tired of being
+serious all the
+time I don’t
+like how you
+treat me like
+we haven’t shared
+the same apartments
+for eighteen years
+long enough to be…
+''')
